cb9b9fdff6ec4a929194d92a5cc53bef2b8fe7aa
bennisan86 <info@bennisan.be>
2019-11-14 12:17:16 +0100

    Final commit, all systems go

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import  { FirebaseContext } from '../Firebase';
+
 const Landing = () => (
-  <div>
-    <h1>Landing</h1>
-  </div>
+  <FirebaseContext.Consumer>
+    {firebase => {
+      return <div>LANDING: I've access to Firebase and render something.</div>;
+    }}
+  </FirebaseContext.Consumer>
 );
 export default Landing;
